571b11e8c243d7307eaa93a57664873e8f62574e
Andrey Melostnoy <andrey.melostnoy@gmail.com>
2020-03-01 18:05:48 +0300

    2-4-7

--- lesson-2-4-step-7.py
+++ lesson-2-4-step-7.py
@@ -1,6 +1,21 @@
-stdin = input()
-stdout = ""
+genome = str(input())
+result = ""
+count = 0
 
+for i in range(len(genome)):
+    if i == (len(genome) - 1):
+        if genome[i] == genome[i - 1]:
+            count += 1
+            result += (str(genome[i]) + str(count))
+        else:
+            count += 1
+            result += (str(genome[i]) + str(count))
+    else:
+        if genome[i] == genome[i + 1]:
+            count += 1
+        else:
+            count += 1
+            result += (str(genome[i]) + str(count))
+            count = 0
 
-
-print(stdout)
+print(result)
--- test.py
+++ test.py
@@ -67,6 +67,24 @@
 #
 # print(index_kek)
 
-genome = "agagagagagggcgscagcgagcagca"
+genome = str(input())
+result = ""
+count = 0
 
-print(genome.count('a'))
+for i in range(len(genome)):
+    if i == (len(genome) - 1):
+        if genome[i] == genome[i - 1]:
+            count += 1
+            result += (str(genome[i]) + str(count))
+        else:
+            count += 1
+            result += (str(genome[i]) + str(count))
+    else:
+        if genome[i] == genome[i + 1]:
+            count += 1
+        else:
+            count += 1
+            result += (str(genome[i]) + str(count))
+            count = 0
+
+print(result)
